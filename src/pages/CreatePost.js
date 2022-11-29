ccd512133cc52aa831ff39da54a16548c7513941
Joaquin <96495159+joaquinjoaco@users.noreply.github.com>
2022-11-29 15:23:49 -0300

    Finished
    users can login and create new posts, and are able to delete theirs only.
    Unauthenticated users can see the Posts but cannot create one without being logged in.

--- src/pages/CreatePost.js
+++ src/pages/CreatePost.js
@@ -1,11 +1,9 @@
-import React, { useEffect, useState } from 'react'
-import { addDoc, collection } from "firebase/firestore"
-import { db, auth } from '../firebase-config';
-import { useNavigate } from 'react-router-dom';
-
+import React, { useState, useEffect } from "react";
+import { addDoc, collection } from "firebase/firestore";
+import { db, auth } from "../firebase-config";
+import { useNavigate } from "react-router-dom";
 
 function CreatePost({ isAuth }) {
-
      const [title, setTitle] = useState("");
      const [postText, setPostText] = useState("");
 
@@ -13,18 +11,19 @@ function CreatePost({ isAuth }) {
      let navigate = useNavigate();
 
      const createPost = async () => {
-          await addDoc(postsCollectionRef, { title, postText, author: { name: auth.currentUser.displayName, id: auth.currentUser.uid } }
-          );
-
-          navigate("/")
-     }
+          await addDoc(postsCollectionRef, {
+               title,
+               postText,
+               author: { name: auth.currentUser.displayName, id: auth.currentUser.uid },
+          });
+          navigate("/");
+     };
 
      useEffect(() => {
           if (!isAuth) {
                navigate("/login");
           }
-     })
-
+     }, []);
 
      return (
           <div className="createPostsPage">
